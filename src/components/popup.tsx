02bfb26ec7b46e8e29797610fc974a3ed766825b
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2023-09-03 00:00:06 +0200

    created popup

---
+++ src/components/popup.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+interface popupProps {
+    children: ReactNode
+}
+
+const Popup = (props: popupProps) => {
+    return <div className="absolute w-screen h-screen bg-black top-0 left-0 opacity-20 flex justify-center items-center">
+        <div className="bg-white p-12">
+        { props.children }
+        </div>
+    </div>
+}
+
+export default Popup
--- src/components/world.tsx
+++ src/components/world.tsx
@@ -3,9 +3,10 @@
 import Storage from "@/services/storage.service";
 import { useEffect, useState } from "react";
 import WorldMap, { DataItem, ISOCode } from "react-svg-worldmap";
+import Popup from "./popup";
 
 const World = () => {
-  const [countries, setCountries] = useState<ICountries>();
+  const [popup, setPopup] = useState<boolean>();
   const [countryData, setCountryData] = useState<DataItem<string>[]>();
 
   useEffect(() => {
@@ -18,10 +19,6 @@ const World = () => {
     const dataFromStorage: ICountries | null = Storage.getData();
 
     console.log("datafromstorage", dataFromStorage);
-
-    if (dataFromStorage) {
-      setCountries(dataFromStorage);
-    }
 
     console.log("countries");
 
@@ -43,23 +40,37 @@ const World = () => {
 
   const countryClicked = () => {
     console.log("fief");
+    setPopup(true);
   };
 
   return (
-    <div className="w-screen h-screen">
-      {countryData ? (
-        <WorldMap
-          color="#000"
-          title="Top 10 Populous Countries"
-          value-suffix="people"
-          size="responsive"
-          data={countryData}
-          onClickFunction={countryClicked}
-        />
+    <>
+      <div className="w-screen h-screen">
+        {countryData ? (
+          <WorldMap
+            color="#000"
+            title="Top 10 Populous Countries"
+            value-suffix="people"
+            size="responsive"
+            data={countryData}
+            onClickFunction={countryClicked}
+          />
+        ) : (
+          <></>
+        )}
+      </div>
+
+      {popup ? (
+        <Popup>
+          <div>Do you want to paint Austria black?</div>
+          <button onClick={() => setPopup(false)}>
+            close
+          </button>
+        </Popup>
       ) : (
         <></>
       )}
-    </div>
+    </>
   );
 };
 
